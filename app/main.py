79c240f3bb6a38aee065ef30f7c853ddefed46fa
JoseAndresVargas <andyxd76@gmail.com>
2025-09-12 16:46:35 -0600

    match word characters with \w implemented

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ import sys
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
-    elif pattern == '\d':
-        for char in input_line:
-            if char.isnumeric():
-                return True
+    elif pattern == "\d":
+        return any(char.isnumeric() for char in input_line)
+    elif pattern == "\w":
+        return any((char.isalnum() or char == "_") for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
