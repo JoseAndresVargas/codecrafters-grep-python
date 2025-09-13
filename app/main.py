134d4ed3f5f8594e7609e626b5ff5b80d09183ae
JoseAndresVargas <andyxd76@gmail.com>
2025-09-13 08:43:05 -0600

    negative character groups implemented

--- app/main.py
+++ app/main.py
@@ -11,18 +11,21 @@ def match_pattern(input_line, pattern):
         return any(char.isnumeric() for char in input_line)
     elif pattern == "\w": # Return True if alphanumeric or _
         return any((char.isalnum() or char == "_") for char in input_line)
-    elif pattern.startswith("["): # Return True if character inside brackets matches
-        pattern_no_brack = pattern.strip("[]")
-        pattern_list = list(pattern_no_brack)
-        return any(char in pattern_list for char in input_line)
+    elif (pattern[0] == "[" and pattern[-1] == "]"): 
+        if pattern[1] == "^": # Return False if character inside brackets matches
+            for char in pattern[2:-1]:
+                if char not in input_line:
+                    return True
+        else: # Return True if character inside brackets matches
+            return any(char in pattern[1:-1] for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
 
 def main():
     pattern = sys.argv[2]
-    print(pattern)
     input_line = sys.stdin.read()
+    print("Logs from your program will appear here!", file=sys.stderr)
 
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
